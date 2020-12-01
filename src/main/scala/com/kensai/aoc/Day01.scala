83109657c6b65642297e104835f8acae207b2329
Vivian Oliveres <v.oliveres@gmail.com>
2020-12-01 09:51:15 +0100

    [DAY01] First challenge

--- src/main/scala/com/kensai/aoc/Day01.scala
+++ src/main/scala/com/kensai/aoc/Day01.scala
@@ -1,5 +1,30 @@
 package com.kensai.aoc
 
-class Day01 {
-  def someMethod(): Boolean = true
+object Day01 {
+
+  /**
+   * Compute the multiplication of the two first numbers from {@code candidates}
+   * that the sum is equal to {@code expectedSum}.
+   *
+   * Return {@code None} if the expected sum cannot be found.
+   */
+  def compute(expectedSum: Long, candidates: List[Long]): Option[Long] = {
+    val sortedCandidates = candidates.sorted
+    var firstIndex = 0
+    var lastIndex = sortedCandidates.size - 1
+
+    while(firstIndex < lastIndex) {
+      val sum = sortedCandidates(firstIndex) + sortedCandidates(lastIndex)
+      if (sum == expectedSum)
+        return Some(sortedCandidates(firstIndex) * sortedCandidates(lastIndex))
+      else {
+        if (sum > expectedSum) lastIndex = lastIndex - 1
+        else  firstIndex = firstIndex + 1
+      }
+
+    }
+
+    None
+
+  }
 }
--- src/test/scala/com/kensai/aoc/Day01Suite.scala
+++ src/test/scala/com/kensai/aoc/Day01Suite.scala
@@ -1,15 +1,70 @@
 package com.kensai.aoc
 
-import org.scalatest.funsuite.AnyFunSuite
+import com.kensai.aoc.Day01.compute
+import org.scalatest._
+import matchers.should.Matchers._
+import flatspec._
 import org.junit.runner.RunWith
 import org.scalatestplus.junit.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
-class Day01Suite extends AnyFunSuite {
+import scala.io.Source
 
-  test("someMethod is always true") {
-    def library = new Day01()
-    assert(library.someMethod)
+@RunWith(classOf[JUnitRunner])
+class Day01Suite extends AnyFlatSpec {
+
+  val ExpectedSum = 2020
+
+  "compute" should "find result from first example" in {
+    // GIVEN: input from specs
+    val input = List(1721L, 979L, 366L, 299L, 675L, 1456L)
+
+    // WHEN: compute
+    val result = compute(ExpectedSum, input)
+
+    // THEN: result is 514579
+    val expectedResult = Some(514579)
+    result should be (expectedResult)
   }
 
+  "compute" should "return empty if there is no result" in {
+    // GIVEN: input from specs
+    val input = List(3L, 5L)
+
+    // WHEN: compute
+    val result = compute(ExpectedSum, input)
+
+    // THEN: result is None
+    result should be (None)
+  }
+
+  "compute" should "return empty for an empty list" in {
+    // GIVEN: input from specs
+    val input: List[Long] = List()
+
+    // WHEN: compute
+    val result = compute(ExpectedSum, input)
+
+    // THEN: result is None
+    result should be (None)
+  }
+
+  "compute" should "find result from input" in {
+    // GIVEN: input from specs
+    val input = readInputFile("src/test/resources/Day01.input")
+
+    // WHEN: compute
+    val result = compute(ExpectedSum, input)
+
+    // THEN: result is defined
+    println(result)
+    result shouldBe defined
+  }
+
+  private def readInputFile(path: String): List[Long] =
+    Source.fromFile(path)
+      .getLines
+      .map(_.trim)
+      .filterNot(_.isEmpty)
+      .map(_.toLong)
+      .toList
 }
