50a3490a9dea9c281083da7c5d4164405aca5a63
Vivian Oliveres <v.oliveres@gmail.com>
2020-12-10 20:00:05 +0100

    [Boyscout] Clean Day 09

--- src/test/scala/com/kensai/aoc/Day09Spec.scala
+++ src/test/scala/com/kensai/aoc/Day09Spec.scala
@@ -15,74 +15,49 @@ class Day09Spec extends AnyFlatSpec with GivenWhenThen {
   private val InputPath = "src/test/resources/Day09.input"
   private val InputSpecPath = "src/test/resources/Day09Spec.input"
 
-
-  "compute for spec" should "return 127" in {
+  "firstInvalidNumber for spec" should "return 127" in {
     Given(s"Input is spec")
     val inputs = readInputFile(InputSpecPath)
 
-    When(s"compute(input)")
-    val result = compute(5, inputs)
+    When(s"firstInvalidNumber(input)")
+    val result = firstInvalidNumber(5, inputs)
 
     Then(s"Result is 127")
     result shouldBe 127L
   }
 
-  "compute for puzzle" should "return solution" in {
+  "firstInvalidNumber for puzzle" should "return solution" in {
     Given(s"Puzzle input")
     val inputs = readInputFile(InputPath)
 
-    When(s"compute(input)")
-    val result = compute(25, inputs)
+    When(s"firstInvalidNumber(input)")
+    val result = firstInvalidNumber(25, inputs)
 
     Then(s"Result is 507622668")
     result shouldBe 507622668L
   }
 
-  "compute2 for spec" should "return 127" in {
+  "findContiguousSet for spec" should "return 62" in {
     Given(s"Input is spec")
     val inputs = readInputFile(InputSpecPath)
 
-    When(s"compute(input)")
-    val result = compute2(5, inputs)
+    When(s"findContiguousSet(input)")
+    val result = findContiguousSet(5, inputs)
 
-    Then(s"Result is 127")
+    Then(s"Result is 62")
     result shouldBe 62L
   }
 
-  "compute2 for spec" should "return XXX" in {
+  "findContiguousSet for spec" should "return solution" in {
     Given(s"Input is spec")
     val inputs = readInputFile(InputPath)
 
-    When(s"compute(input)")
-    val result = compute2(25, inputs)
+    When(s"findContiguousSet(input)")
+    val result = findContiguousSet(25, inputs)
 
     Then(s"Result is 76688505")
     result shouldBe 76688505L
   }
-
-  //*********************************************************
-
-//  "compute2 for spec" should "return 8" in {
-//    Given(s"Input is spec")
-//    val inputs = readInputFile(InputSpecPath)
-//
-//    When(s"accumulatorAfterFixingInputs(input)")
-//    val result = compute2(inputs)
-//
-//    Then(s"Result is 8")
-//    result shouldBe 8
-//  }
-//
-//  "compute2 for puzzle" should "return solution" in {
-//    Given(s"Puzzle solution")
-//    val inputs = readInputFile(InputPath)
-//
-//    When(s"accumulatorAfterFixingInputs(input)")
-//    val result = compute2(inputs)
-//
-//    Then(s"Result is 1984")
-//    result shouldBe 1984
-//  }
 
   private def readInputFile(path: String): List[String] =
     Source.fromFile(path)
