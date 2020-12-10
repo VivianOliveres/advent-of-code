4c4a552aed0c9f5d27aa6c0725dfeb26a0c3464d
Vivian Oliveres <v.oliveres@gmail.com>
2020-12-10 20:00:05 +0100

    [Boyscout] Clean Day 09

--- src/main/scala/com/kensai/aoc/Day09.scala
+++ src/main/scala/com/kensai/aoc/Day09.scala
@@ -2,49 +2,43 @@ package com.kensai.aoc
 
 object Day09 {
 
-  //TODO: clean everything later
-  
-  def doCompute(n: Int, input: List[Long]): Option[Long] = {
-    val toto = input
-      .combinations(2)
-      .find(_.sum == input(n))
-    if (toto.isDefined) None
-    else Some(input(n))
+  private def parse(inputs: List[String]) =
+    inputs.map(_.trim).filterNot(_.isEmpty).map(_.toLong)
+
+  /**
+   * Return true if there is no combinations of 2 elements in `subValues` that is equal to `expectedNumber`.
+   */
+  private def isInvalid(expectedNumber: Long, subValues: List[Long]): Boolean =
+    !subValues.combinations(2).exists(_.sum == expectedNumber)
+
+  /**
+   * Find the first number that is not the sum of 2 of the `window` elements.
+   */
+  def firstInvalidNumber(window: Int, inputs: List[String]): Long =
+    doFirstInvalidNumber(window, parse(inputs))
+
+  private def doFirstInvalidNumber(window: Int, values: List[Long]): Long =
+    (0 until values.size - window - 1)
+      .map(i => (values(i + window), values.slice(i, i + window )))
+      .filter { case (expectedNumber, subList) => isInvalid(expectedNumber, subList) }
+      .map(_._1)
+      .head
+
+  /**
+   * Find the first contiguous set where the sum is equal to `firstInvalidNumber` and return the `min+max` of this set.
+   */
+  def findContiguousSet(window: Int, inputs: List[String]): Long = {
+    val values = parse(inputs)
+    val expected = doFirstInvalidNumber(window, values)
+    val contiguousSet = (0 until values.size - window)
+      .flatMap(doFindContiguousSet(_, expected, values))
+      .head
+    contiguousSet.min + contiguousSet.max
   }
 
-  def compute(n: Int, inputs: List[String]): Long = {
-    val values = inputs.map(_.trim).filterNot(_.isEmpty).map(_.toLong)
-    (0 until inputs.size - n).map(i => values.slice(i, i + n + 1 )).flatMap(s => {
-      doCompute(n, s)
-    }).head
-  }
-
-  def compute2(n: Int, inputs: List[String]): Long = {
-    val values = inputs.map(_.trim).filterNot(_.isEmpty).map(_.toLong)
-    (0 until inputs.size - n).map(i => values.slice(i, i + n + 1 )).flatMap(s => {
-      val r = doCompute(n, s)
-      r.map(pbValue => {
-        println(s"s[$s]")
-        (0 until inputs.size - n).map(doCompute2(_, pbValue, values)).filter(_ > 0).head
-      })
-    }).head
-  }
-
-  def doCompute2(currentIndex: Int, pbValue: Long, values: List[Long]): Long = {
-    if (currentIndex + 1 >= values.size) return -1
-
-    var tmpSum = values(currentIndex) + values(currentIndex + 1)
-    var index = currentIndex + 1
-    while(values(index) < pbValue) {
-      val slice = values.slice(currentIndex, index)
-//      println(s"pbValue[$pbValue] slice[$slice] index[$index] tmpSum[$tmpSum]")
-      if (tmpSum == pbValue) return slice.min + slice.max
-      index = index + 1
-      tmpSum = tmpSum + values(index)
-    }
-
-    -1
-  }
-
+  def doFindContiguousSet(currentIndex: Int, expectedSum: Long, values: List[Long]): Option[List[Long]] =
+    (2 to values.size)
+      .map(i => values.slice(currentIndex, currentIndex + i))
+      .find(_.sum == expectedSum)
 
 }
