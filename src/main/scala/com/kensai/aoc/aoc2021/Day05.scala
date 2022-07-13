a969df9e32cdd3a8a452ed124b0769f21921c6e1
Vivian Oliveres <vivian@zen.ly>
2022-07-13 10:42:35 +0200

    AoC 2021 - Day 05

---
+++ src/main/scala/com/kensai/aoc/aoc2021/Day05.scala
@@ -0,0 +1,57 @@
+package com.kensai.aoc.aoc2021
+
+object Day05 {
+
+  case class VentLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+    def generatePoints: Seq[VentPoint] =
+      if (x1 == x2)
+        range(y1, y2).map(y => VentPoint(x1, y))
+      else if (y1 == y2)
+        range(x1, x2).map(x => VentPoint(x, y1))
+      else
+        for {
+          (x, y) <- range(x1, x2) zip range(y1, y2)
+        } yield VentPoint(x, y)
+
+    private def range(val1: Int, val2: Int): Range = {
+      val step = if (val1 < val2) 1 else -1
+      val1 to val2 by step
+    }
+  }
+
+  case class VentPoint(x: Int, y: Int)
+
+  private val lineRegex = """(\d+),(\d+) -> (\d+),(\d+)""".r
+  private def parse(inputs: Seq[String]): Seq[VentLine] =
+    inputs.filterNot(_.isEmpty).map(_.trim).flatMap { input =>
+      input match {
+        case lineRegex(x1, y1, x2, y2) =>
+          Some(VentLine(x1.toInt, y1.toInt, x2.toInt, y2.toInt))
+        case _ => None
+      }
+    }
+
+  /**
+    * Count the number of dangerous points (ie: points crossed by at least to VentLine).
+    * But count only the Vertical and Horizontal lines.
+    */
+  def countVHDangerousPoints(inputs: Seq[String]): Int =
+    countDangerousPoints(inputs, line => line.x1 == line.x2 || line.y1 == line.y2)
+
+  private def countDangerousPoints(inputs: Seq[String], filter: VentLine=>Boolean): Int = {
+    val lines = parse(inputs).filter(filter)
+    val points = lines.flatMap(_.generatePoints)
+    val countByPoint =
+      points.groupBy(point => (point.x, point.y)).map{case (key, value) => (key, value.size)}
+    countByPoint.count(_._2 > 1)
+  }
+
+  /**
+    * Count the number of dangerous points (ie: points crossed by at least to VentLine).
+    * Count Vertical, Horizontal and Diagonal lines.
+    */
+  def countAllDangerousPoints(inputs: Seq[String]): Int = {
+    countDangerousPoints(inputs, _ => true)
+  }
+
+}
