f2d891a69d27b83d16f01417b3cb8677805fc7ec
Vivian Oliveres <vivian@zen.ly>
2022-10-06 13:37:41 +0200

    [AoC 2021] Day 20

---
+++ src/test/scala/com/kensai/aoc/aoc2021/Day20Spec.scala
@@ -0,0 +1,88 @@
+package com.kensai.aoc.aoc2021
+
+import com.kensai.aoc.aoc2021.Day20._
+import com.kensai.aoc.lib.Lib.readInputLines
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should.Matchers._
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Day20Spec extends AnyFlatSpec with GivenWhenThen {
+
+  private lazy val puzzleInput = readInputLines(
+    "src/test/resources/2021/Day20.input"
+  )
+
+  private lazy val puzzleSpecInput = readInputLines(
+    "src/test/resources/2021/Day20Spec.input"
+  )
+
+    "parse" should "find result from spec input" in {
+      Given("Puzzle spec input")
+
+      When("parse(puzzleSpecInput)")
+      val result = parse(puzzleSpecInput)
+
+      Then("Result is expected")
+      result.image.points should have size 25
+      result.enhancementAlgorithm should have size 512
+
+      result.image.points(Point(0, 0)) shouldBe true
+      result.image.points(Point(3, 0)) shouldBe true
+      result.image.points(Point(0, 1)) shouldBe true
+      result.image.points(Point(2, 1)) shouldBe false
+      result.image.points(Point(0, 2)) shouldBe true
+      result.image.points(Point(1, 2)) shouldBe true
+      result.image.points(Point(4, 2)) shouldBe true
+      result.image.points(Point(2, 3)) shouldBe true
+      result.image.points(Point(2, 4)) shouldBe true
+      result.image.points(Point(3, 4)) shouldBe true
+      result.image.points(Point(4, 4)) shouldBe true
+    }
+
+  "countPixelsLit(2)" should "find result from spec input" in {
+    Given("Puzzle spec input")
+    val input = parse(puzzleSpecInput)
+
+    When("countPixelsLit(puzzleSpecInput)")
+    val result = countPixelsLit(input, 2)
+
+    Then("Result is 35")
+    result shouldBe 35
+  }
+
+  "countPixelsLit(2)" should "find result from input" in {
+    Given("Puzzle spec input")
+    val input = parse(puzzleInput)
+
+    When("countPixelsLit(puzzleSpecInput)")
+    val result = countPixelsLit(input, 2)
+
+    Then("Result is expected")
+    result shouldBe 5503
+  }
+
+  "countPixelsLit(50)" should "find result from spec input" in {
+    Given("Puzzle spec input")
+    val input = parse(puzzleSpecInput)
+
+    When("countPixelsLit(puzzleSpecInput)")
+    val result = countPixelsLit(input, 50)
+
+    Then("Result is 3351")
+    result shouldBe 3351
+  }
+
+  "countPixelsLit(50)" should "find result from input" in {
+    Given("Puzzle spec input")
+    val input = parse(puzzleInput)
+
+    When("countPixelsLit(puzzleSpecInput)")
+    val result = countPixelsLit(input, 50)
+
+    Then("Result is expected")
+    result shouldBe 19156
+  }
+}
