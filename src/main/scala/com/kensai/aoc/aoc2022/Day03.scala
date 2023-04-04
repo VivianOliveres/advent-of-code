dc56bce5affff51e8a94faa84dfe34940f172e4b
Vivian Oliveres <v.oliveres@gmail.com>
2023-04-04 12:06:58 +0200

    [AoC 2022] Day 03

---
+++ src/main/scala/com/kensai/aoc/aoc2022/Day03.scala
@@ -0,0 +1,38 @@
+package com.kensai.aoc.aoc2022
+
+object Day03 {
+
+  def findSharedItem(line: String): Char = {
+    val (left, right) = line.splitAt(line.length / 2)
+    val result        = left.toSet.intersect(right.toSet)
+    result.head
+  }
+
+  def sumPrioritiesByElf(elves: Seq[String]): Int =
+    elves
+      .map(_.trim)
+      .filterNot(_.isEmpty)
+      .map(findSharedItem)
+      .map(toPriority)
+      .sum
+
+  private def toPriority(c: Char): Int =
+    if (c.isLower)
+      c.toInt - 96
+    else
+      c.toInt - 38
+
+  def findSharedItem(elves: Seq[String]): Char = {
+    val result = elves.foldLeft(elves.head.toSet) { case (acc, other) =>
+      acc.intersect(other.toSet)
+    }
+    result.head
+  }
+
+  def sumPrioritiesByGroup(elves: Seq[String]): Int =
+    elves
+      .grouped(3)
+      .map(findSharedItem)
+      .map(toPriority)
+      .sum
+}
