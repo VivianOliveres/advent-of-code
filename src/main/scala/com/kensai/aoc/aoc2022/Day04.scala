beca60e68895b2c1dc79c3c4d54c0cb3e33a6a64
Vivian Oliveres <v.oliveres@gmail.com>
2023-04-04 13:26:28 +0200

    [AoC 2022] Day 04

---
+++ src/main/scala/com/kensai/aoc/aoc2022/Day04.scala
@@ -0,0 +1,29 @@
+package com.kensai.aoc.aoc2022
+
+object Day04 {
+
+  case class RangeDay4(leftMin: Int, leftMax: Int, rightMin: Int, rightMax: Int) {
+    def isSelfIncluded: Boolean =
+      (leftMin <= rightMin && leftMax >= rightMax) ||
+      (rightMin <= leftMin && rightMax >= leftMax)
+
+    def isOverlapped: Boolean =
+      leftMax >= rightMin && leftMin <= rightMax
+  }
+
+  private val rowRegex = """(\d+)-(\d+),(\d+)-(\d+)""".r
+  def parse(lines: Seq[String]): Seq[RangeDay4] =
+    lines.map(_.trim).filterNot(_.isEmpty).map {
+      case rowRegex(leftMinStr, leftMaxStr, rightMinStr, rightMaxStr) =>
+        RangeDay4(leftMinStr.toInt, leftMaxStr.toInt, rightMinStr.toInt, rightMaxStr.toInt)
+      case str => throw new IllegalArgumentException(s"Invalid line [$str]")
+    }
+
+  def countSelfIncluded(groups: Seq[RangeDay4]): Int =
+    groups.count(_.isSelfIncluded)
+
+  def countOverlapped(groups: Seq[RangeDay4]): Int =
+    groups.count(_.isOverlapped)
+
+
+}
