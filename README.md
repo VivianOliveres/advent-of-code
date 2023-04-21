56e55a82e6d6b3ee343fe6ba90f0ac12108a8e5f
Vivian Oliveres <v.oliveres@gmail.com>
2023-04-21 10:56:24 +0200

    [AoC 2022] Day 15

--- README.md
+++ README.md
@@ -29,6 +29,7 @@
 | 12  | [Hill Climbing Algorithm](https://adventofcode.com/2022/day/12) | [Day12](../master/src/main/scala/com/kensai/aoc/aoc2022/Day12.scala) | [Day12Spec](../master/src/test/scala/com/kensai/aoc/aoc2022/Day12Spec.scala) | [](../master/src/test/scala/com/kensai/aoc/aoc2022/Day12PropSpec.scala)              | [Day12.input](../master/src/test/resources/aoc2022/Day12.input) | 
 | 13  | [Distress Signal](https://adventofcode.com/2022/day/13)         | [Day13](../master/src/main/scala/com/kensai/aoc/aoc2022/Day13.scala) | [Day13Spec](../master/src/test/scala/com/kensai/aoc/aoc2022/Day13Spec.scala) | [](../master/src/test/scala/com/kensai/aoc/aoc2022/Day13PropSpec.scala)              | [Day13.input](../master/src/test/resources/aoc2022/Day13.input) | 
 | 14  | [Regolith Reservoir](https://adventofcode.com/2022/day/14)      | [Day14](../master/src/main/scala/com/kensai/aoc/aoc2022/Day14.scala) | [Day14Spec](../master/src/test/scala/com/kensai/aoc/aoc2022/Day14Spec.scala) | [](../master/src/test/scala/com/kensai/aoc/aoc2022/Day14PropSpec.scala)              | [Day14.input](../master/src/test/resources/aoc2022/Day14.input) | 
+| 15  | [Beacon Exclusion Zone](https://adventofcode.com/2022/day/15)   | [Day15](../master/src/main/scala/com/kensai/aoc/aoc2022/Day15.scala) | [Day15Spec](../master/src/test/scala/com/kensai/aoc/aoc2022/Day15Spec.scala) | [](../master/src/test/scala/com/kensai/aoc/aoc2022/Day15PropSpec.scala)              | [Day15.input](../master/src/test/resources/aoc2022/Day15.input) | 
 
 
 ## [AOC 2021](https://adventofcode.com/2021)
---
+++ src/main/scala/com/kensai/aoc/aoc2022/Day15.scala
@@ -0,0 +1,90 @@
+package com.kensai.aoc.aoc2022
+
+import com.kensai.aoc.lib.Geo.Point2D
+
+object Day15 {
+
+  case class Sensor(pos: Point2D, beacon: Point2D)
+
+  private val rowRegex = """Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)""".r
+  def parse(lines: Seq[String]): Set[Sensor] =
+    lines
+      .map(_.trim)
+      .filterNot(_.isEmpty)
+      .map {
+        case rowRegex(posX, posY, beaconX, beaconY) => Sensor(Point2D(posX.toInt, posY.toInt), Point2D(beaconX.toInt, beaconY.toInt))
+        case str                                    => throw new IllegalAccessException(s"Invalid line [$str]")
+      }
+      .toSet
+
+  case class RangeDay15(minX: Int, maxX: Int) {
+    def count: Int =
+      math.abs(maxX - minX) + 1
+  }
+
+  private def computeRanges(sensors: Set[Sensor], yTarget: Int): Seq[RangeDay15] =
+    (for {
+      sensor <- sensors
+      manDist  = math.abs(sensor.pos.x - sensor.beacon.x) + math.abs(sensor.pos.y - sensor.beacon.y)
+      xManDist = manDist - math.abs(sensor.pos.y - yTarget) if xManDist >= 0
+    } yield {
+      val min = math.min(sensor.pos.x - xManDist, sensor.pos.x + xManDist)
+      val max = math.max(sensor.pos.x - xManDist, sensor.pos.x + xManDist)
+      RangeDay15(min, max)
+    }).toSeq
+
+  private def mergeRanges(ranges: Seq[RangeDay15]): Seq[RangeDay15] =
+    ranges.sortBy(_.minX).foldLeft(List.empty[RangeDay15]) {
+      case (prev :: tl, current) if current.minX <= prev.maxX + 1 =>
+        RangeDay15(prev.minX, math.max(prev.maxX, current.maxX)) :: tl
+      case (acc, cur) =>
+        cur :: acc
+    }
+
+  def countInvalidPositions(sensors: Set[Sensor], yTarget: Int): Int = {
+    val ranges = computeRanges(sensors, yTarget)
+
+    val xBeacons = sensors.filter(_.beacon.y == yTarget).map(_.beacon.x)
+    val rangesWithoutBeacons = xBeacons.foldLeft(ranges) { case (acc, xBeacon) =>
+      acc.flatMap { range =>
+        if (range.minX == xBeacon)
+          Seq(RangeDay15(xBeacon + 1, range.maxX))
+        else if (range.maxX == xBeacon)
+          Seq(RangeDay15(range.minX, xBeacon - 1))
+        else if (range.minX < xBeacon && range.maxX > xBeacon)
+          Seq(RangeDay15(range.minX, xBeacon - 1), RangeDay15(xBeacon + 1, range.maxX))
+        else
+          Seq(range)
+      }
+    }
+
+    val allRanges = mergeRanges(rangesWithoutBeacons)
+    allRanges
+      .map(_.count)
+      .sum
+  }
+
+  def findValidPosition(sensors: Set[Sensor], yTarget: Int, maxValue: Int): Option[Point2D] = {
+    val ranges       = computeRanges(sensors, yTarget)
+    val mergedRanges = mergeRanges(ranges)
+    mergedRanges
+      .find(range => range.minX > 0 || range.maxX < maxValue)
+      .map { range =>
+        if (range.minX > 0)
+          range.minX - 1
+        else
+          range.maxX + 1
+      }
+      .map(x => Point2D(x, yTarget))
+  }
+
+  def tuningFrequency(sensors: Set[Sensor], maxValue: Int): Long = {
+    val result = (0 to maxValue) // TODO: it takes 26s to find solution
+      .map(findValidPosition(sensors, _, maxValue))
+      .find(_.isDefined)
+      .get
+      .get
+    result.x * 4000000L + result.y
+  }
+
+}
